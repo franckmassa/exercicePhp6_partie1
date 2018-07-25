9cd2f16a1351c846f03c28cbb7bf75127e7637cd
massa <massafranck@gmail.com>
2018-07-25 21:10:13 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$name = 'Maurice';
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice6</title>
+  </head>
+  <body>
+<p>
+<?php
+echo 'Bonjour '.$name.', comment vas tu?';
+ ?>
+</p>
+  </body>
+</html>
